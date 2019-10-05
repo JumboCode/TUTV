6e9ac713b1cb4bae010989dfbcf3aa4cd42b302c
Deepanshu Utkarsh <deepanshu.utkarsh@tufts.edu>
2019-10-05 13:52:50 -0400

    Create hello world example

--- src/frontend/src/Hello.tsx
+++ src/frontend/src/Hello.tsx
@@ -1,21 +1,7 @@
-import React, { useState } from 'react'; // import useState Hook from React
+import React from 'react';
 
 const Hello: React.FC = () => {
-  // can use hooks here.. "hook into" React features
-  // useState is a Hook that lets you add React state to function components
-
-  // Declare a new state variable, which we'll call "counter", and set it to 0. This will hold the number of button clicks. setCounter is a function that lets us update the counter
-  const [counter, setCounter] = useState(0);
-
-  return (
-    //when the user clicks, we call setCounter with a value incremented by 1.
-    <div>
-      <p>Hello, world!!!!!!</p>
-      <p>You clicked {counter} times</p>
-
-      <button onClick={() => setCounter(counter + 1)}>Click me</button>
-    </div>
-  );
+  return <p>Hello, world!</p>;
 };
 
 export default Hello;
