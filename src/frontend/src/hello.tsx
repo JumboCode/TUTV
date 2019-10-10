285c39b15d993bb0f3a6f532bad6365479f514c2
Daeseob Lim <thedaysob@gmail.com>
2019-10-10 09:07:38 -0400

    commiting f/hello-world branch

--- src/frontend/src/hello.tsx
+++ src/frontend/src/hello.tsx
@@ -1,4 +1,9 @@
-#here is our code 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+import React from 'react';
+
+const Welcome: React.FC = () => {
+	return (
+		<h1>Hello world</h1>
+	);
 }
+
+export default Welcome;
