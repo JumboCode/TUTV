3e0b596c7d43cecff1dc2d28a53f837009df9e4c
Daniel Ryaboshapka <dan4821@gmail.com>
2019-10-14 15:24:58 -0400

    added button/counter

--- src/frontend/src/Hello.tsx
+++ src/frontend/src/Hello.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Hello: React.FC = () => {
-  return <p>Hello, world!</p>;
+  const [count, setCount] = useState(0);
+
+  return (
+  	<div>
+  		<p>Drybell</p>
+  		<p>The counter was clicked {count} times</p>
+  		<button onClick={() => setCount(count + 1)}>
+  			CLICK HERE!!! 
+  		</button>
+  	</div>
+  )
 };
 
 export default Hello;
