33aa84b3c884cc76fd179e1f430fdc8ff64f8708
Jiayi Ma <jiayi.ma@tufts.edu>
2019-10-14 14:10:02 -0400

    Added button using state hooks

--- src/frontend/src/Hello.tsx
+++ src/frontend/src/Hello.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Hello: React.FC = () => {
-  return <p>Hello, world!</p>;
+  const [counter, setCounter] = useState(0); // initialize the count to 0
+  return (
+    <div>
+      <p>Hello, world! The counter was clicked {counter} times. </p>
+      <button onClick={() => setCounter(counter + 1)}>Click me</button>
+    </div>
+  );
 };
 
 export default Hello;
