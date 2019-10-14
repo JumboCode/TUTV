a43c723b708f2663f894c7b70fc86d6e1f17dcd7
Xenia <xenia.rudchenko@tufts.edu>
2019-10-13 23:45:56 -0400

    "Added counter to Hello component"

--- src/frontend/src/Hello.tsx
+++ src/frontend/src/Hello.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Hello: React.FC = () => {
-  return <p>Hello, world!</p>;
-};
+  const [counter, setCounter] = useState(0);
 
+  return (
+    <div>
+      <p>Hello, world!</p>
+      <p>You clicked {counter} times</p>
+      <button onClick={() => setCounter(counter + 1)}>Click me</button>
+    </div>
+  );
+};
 export default Hello;
