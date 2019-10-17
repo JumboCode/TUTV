e63e31ee605e3253039f3de391816e80ffe71ecb
emai-lai <emai.lai@tufts.edu>
2019-10-17 18:30:05 -0400

    copy for xenia

---
+++ src/frontend/src/components/TextField.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// class TextField extends React.Component {
+
+// function TextField() {
+// 	return (
+// 		<View>
+// 			<Image alt="alt text goes here" src="exampleImage.png" />
+// 		</View>
+// 	);
+// }
