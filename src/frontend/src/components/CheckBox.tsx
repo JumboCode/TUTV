d93b08d2ef4a94aa41f898f4f0a5280e22c0fff5
emai-lai <emai.lai@tufts.edu>
2019-10-17 18:30:05 -0400

    copy for xenia

--- src/frontend/src/components/CheckBox.tsx
+++ src/frontend/src/components/CheckBox.tsx
@@ -1,15 +1,19 @@
-function Checkbox({ checked, label, handleClick })
-{
-	return
-	(
-	   <div
-	      className={checked ? 'Checkbox-container checked' : 'Checkbox-container'}
-	      onClick={handleClick}
-	      role="button"
-	      tabIndex={0}
-	      data-label={label}
-	   >
-	      <p className="label" data-label={label}>{label</p>
-	   </div>
-   )
-};
+// import React from 'react';
+
+// function Checkbox({ checked, label, handleClick })
+// {
+// 	return
+// 	(
+// 	   <div
+// 	      className={checked ? 'Checkbox-container checked' : 'Checkbox-container'}
+// 	      onClick={handleClick}
+// 	      role="button"
+// 	      tabIndex={0}
+// 	      data-label={label}
+// 	   >
+// 	      <p className="label" data-label={label}>{label</p>
+// 	   </div>
+//    )
+// };
+
+// export default Checkbox;
---
+++ src/frontend/src/components/Link.tsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+
+// const Link: React.FC = () => {
+// {
+// 	return(
+
+// 	);
+// }
+
+// export default Link;
