56d313656ebd646b0e3bac324e0fffa26bd93b7c
Erena Inoue <ErenaCInoue@Erenas-MacBook-Pro.local>
2019-10-20 16:53:58 -0400

    component rectangles added

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Hello from "./helloworld";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Editing...</p>
+        <Hello></Hello>
         <a
           className="App-link"
           href="https://reactjs.org"
