08b7039d83dbdfe5a77078c485159995de4c4981
Xenia <xenia.rudchenko@tufts.edu>
2019-10-25 11:35:51 -0400

    Basic component files edited with Prettier

--- src/frontend/src/components/Link/Link.tsx
+++ src/frontend/src/components/Link/Link.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Link: React.FC = () => {
-	return (
-		<div>
-			<nav>
-				<a href="https://google.com">GOOGLE LINK</a>
-			</nav>
-		</div>
-	);
+  return (
+    <div>
+      <nav>
+        <a href="https://google.com">GOOGLE LINK</a>
+      </nav>
+    </div>
+  );
 };
 
 export default Link;
--- src/frontend/src/components/TextField/TextField.tsx
+++ src/frontend/src/components/TextField/TextField.tsx
@@ -1,19 +1,7 @@
-// import React from 'react';
-
-// class TextField extends React.Component {
-
-// function TextField() {
-// 	return (
-// 		<View>
-// 			<Image alt="alt text goes here" src="exampleImage.png" />
-// 		</View>
-// 	);
-// }
-
 import React from 'react';
 
 const TextField: React.FC = () => {
-	return <div>TextField Example</div>;
+  return <div>TextField Example</div>;
 };
 
 export default TextField;
