09c9e6d3ae44650fa20283e2d53a680674a13f75
Luke Taylor <luke@deentaylor.com>
2019-10-29 18:15:01 -0400

    Add created_date to equipmentitem model

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+
 
 class EquipmentType(models.Model):
     name = models.CharField(max_length=100)
@@ -6,6 +8,15 @@ class EquipmentType(models.Model):
 class EquipmentItem(models.Model):
     equipment_type = models.ForeignKey(EquipmentType, on_delete=models.CASCADE)
     comments = models.CharField(max_length=200)
+    created_at = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        # Runs on first create (model will have id after its initial creation)
+        if not self.id:
+            self.created_at = timezone.now()
+        # Let default save handler do its thing at the end
+        return super(EquipmentItem, self).save(*args, **kwargs)
+
 
 class EquipmentRequest(models.Model):
     start_date = models.DateField()
