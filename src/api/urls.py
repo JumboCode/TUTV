320267ba2e6a3d2637e873388e90551f1c75282e
Daeseob Lim <thedaysob@gmail.com>
2019-11-13 22:59:06 -0500

    Get equipment type list api call implmented.

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,16 +1,19 @@
 from django.urls import include, path, re_path
 from rest_framework import routers
 import api.views as views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'equipment-types', views.EquipmentTypeViewSet)
 router.register(r'equipment-items', views.EquipmentItemViewSet)
 
+# router.register(r'test', views.EquipmentTypeList.as_view(), base_name='test')
 
 urlpatterns = [
     # ... the rest of the urlpatterns ...
     # must be catch-all for pushState to work
     *router.urls,
+    path('snippets/', views.EquipmentTypeList.as_view()),
     re_path(r'^', views.FrontendAppView.as_view()),
 ]
--- src/api/views/equipment.py
+++ src/api/views/equipment.py
@@ -1,7 +1,16 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from api.models import EquipmentType, EquipmentItem
 from rest_framework import viewsets
 from api.serializers import EquipmentTypeSerializer, EquipmentItemSerializer
 
+class EquipmentTypeList(APIView):
+    def get(self, request, format=None):
+        equipments = EquipmentType.objects.all()
+        serializer = EquipmentTypeSerializer(equipments, many=True, context={'request': request})
+        return Response(serializer.data)
 
 class EquipmentTypeViewSet(viewsets.ModelViewSet):
     """
@@ -16,4 +25,4 @@ class EquipmentItemViewSet(viewsets.ModelViewSet):
     API endpoint that allows Equipment Items to be viewed or edited.
     """
     queryset = EquipmentItem.objects.all()
-    serializer_class = EquipmentItemSerializer
+    serializer_class = EquipmentItemSerializer    
