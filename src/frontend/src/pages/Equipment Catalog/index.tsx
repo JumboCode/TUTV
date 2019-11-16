beba88dec270fb215cc5bbd29dd05e94748c4598
Nivedita Nath <nivi@Peters-MacBook-Pro-2.local>
2019-11-16 11:35:25 -0500

    equip

---
+++ src/frontend/src/pages/Equipment Catalog/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EquipmentBrowser: React.FC = () => {
+  return (
+    <div>
+      <h1>Equipment Browser page</h1>
+    </div>
+  );
+};
+
+export default EquipmentBrowser;
