c03eb9d41289e90a00ceaa44c86cd7453a84625a
Isabella Urdahl <isabella.urdahl@gmail.com>
2019-11-23 13:50:17 -0500

    Button Props

--- src/frontend/src/components/Button/Button.tsx
+++ src/frontend/src/components/Button/Button.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Button: React.FC = () => {
-  return <button>Click me</button>;
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick: () => void;
+};
+
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+  return <button onClick={onClick}>{children}</button>;
 };
 
 export default Button;
