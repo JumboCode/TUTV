801a19701c6021111f8775fbe01fea9d9a6f2e37
Xenia <xenia.rudchenko@tufts.edu>
2020-02-10 17:13:09 -0500

    Populate Equipment Browser page with real data

--- client/src/pages/EquipmentBrowser/EquipmentBrowser.tsx
+++ client/src/pages/EquipmentBrowser/EquipmentBrowser.tsx
@@ -1,22 +1,85 @@
 import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import styles from './EquipmentBrowser.module.css';
 
+export interface Item {
+  name: string;
+  slug: string;
+  category: {
+    name: string;
+    slug: string;
+    id: number;
+  };
+  total_count: number;
+  image: string | null;
+}
+
 const EquipmentBrowser: React.FC = () => {
+  const [items, setItems] = React.useState<Array<Item>>([]);
+  const listItems = items.map(item => (
+    <div className={styles.equipment} key={item.name}>
+      <div>
+        <img src="https://via.placeholder.com/150" />
+      </div>
+      <div>{item.name}</div>
+    </div>
+  ));
+  React.useEffect(() => {
+    fetch('https://tutv-mock.now.sh/api/v1/equipment/')
+      .then(response => response.json())
+      .then(response => setItems(response.data))
+      .catch(error => console.error(error));
+  }, []);
   return (
-    <div className={styles.header}>
-      <div className={styles.timepicker}>
-        <div>
-          <div>Checkout Time</div>
-          <input type="datetime-local" />
+    <div>
+      <div className={styles.header}>
+        <div className={styles.timepicker}>
+          <div>
+            <div>Checkout Time</div>
+            <input type="datetime-local" />
+          </div>
+          <div>
+            <div>Return Time</div>
+            <input type="datetime-local" />
+          </div>
         </div>
         <div>
-          <div>Return Time</div>
-          <input type="datetime-local" />
+          <button>Cancel Request</button>
+          <button>Continue</button>
         </div>
       </div>
       <div>
-        <button>Cancel Request</button>
-        <button>Continue</button>
+        <div className={styles.equipmentPage}>
+          <div className={styles.equipmentGrid}>
+            <Tabs>
+              <TabList>
+                <Tab>Camera</Tab>
+                <Tab>Audio</Tab>
+                <Tab>Lighting</Tab>
+                <Tab>Misc</Tab>
+              </TabList>
+
+              <TabPanel>
+                <div className={styles.equipmentContainer}>{listItems}</div>
+              </TabPanel>
+              <TabPanel>
+                <div className={styles.equipmentContainer}>{listItems}</div>
+              </TabPanel>
+              <TabPanel>
+                <div className={styles.equipmentContainer}>{listItems}</div>
+              </TabPanel>
+              <TabPanel>
+                <div className={styles.equipmentContainer}>{listItems}</div>
+              </TabPanel>
+            </Tabs>
+          </div>
+          <div className={styles.selectedEquipment}>
+            <div>
+              <div>Selected Equipment</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
