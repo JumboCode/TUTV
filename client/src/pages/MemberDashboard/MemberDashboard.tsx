6601ebda470db63a087cc5d3e88769ca024578e5
Isabella Urdahl <isabella.urdahl@gmail.com>
2020-02-23 20:01:59 -0500

    updates to member dashboard

--- client/src/pages/MemberDashboard/MemberDashboard.tsx
+++ client/src/pages/MemberDashboard/MemberDashboard.tsx
@@ -9,47 +9,88 @@ const MemberDashboard: React.FC = () => {
         <button className={styles.newreqbutton}>New Request</button>
       </div>
 
-      <div className={styles.requestbox}>
+      <div>
         <h2 className={styles.requestheader}>My current requests</h2>
-        <div className={styles.menubar}>
-          <p className={styles.menuitem}> Project Name </p>
-          <p className={styles.menuitem}> Checkout Time </p>
-          <p className={styles.menuitem}>Return Time </p>
-          <p className={styles.menuitem}> Status </p>
-        </div>
-
-        <div>
-          <div className={styles.projectreq}>
-            <h3>Request for Current Project 1</h3>
-            <button className={styles.viewbutton}> View</button>
-          </div>
-          <div className={styles.projectreq}>
-            <h3>Request for Current Project 2</h3>
-            <button className={styles.viewbutton}> View</button>
-          </div>
-        </div>
+        <table className={styles.reqtable}>
+          <tr>
+            <th>Project Name</th>
+            <th>Checkout Time</th>
+            <th>Return Time</th>
+            <th>Status</th>
+          </tr>
+          <tr className={styles.projectreq}>
+            <td>Fake Project Name 1</td>
+            <td>02/23/20 5:00pm</td>
+            <td>03/23/20 5:00pm</td>
+            <td>Pending</td>
+            <td>
+              <button className={styles.viewbutton}> View</button>
+            </td>
+            <td>
+              <button className={styles.editbutton}> Edit</button>
+            </td>
+          </tr>
+          <tr>
+            <th>Project Name</th>
+            <th>Checkout Time</th>
+            <th>Return Time</th>
+            <th>Status</th>
+          </tr>
+          <tr className={styles.projectreq}>
+            <td>Fake Project Name 2</td>
+            <td>02/24/20 3:00pm</td>
+            <td>03/24/20 4:00pm</td>
+            <td>Approved</td>
+            <td>
+              <button className={styles.viewbutton}> View</button>
+            </td>
+            <td>
+              <button className={styles.editbutton}> Edit</button>
+            </td>
+          </tr>
+        </table>
       </div>
 
-      <div className={styles.requestbox}>
+      <div>
         <h2 className={styles.requestheader}>My past requests</h2>
-
-        <div className={styles.menubar}>
-          <p className={styles.menuitem}> Project Name </p>
-          <p className={styles.menuitem}> Checkout Time </p>
-          <p className={styles.menuitem}>Return Time </p>
-          <p className={styles.menuitem}> Status </p>
-        </div>
-
-        <div>
-          <div className={styles.projectreq}>
-            <h3>Request for Past Project 1</h3>
-            <button className={styles.viewbutton}> View</button>
-          </div>
-          <div className={styles.projectreq}>
-            <h3>Request for Past Project 2</h3>
-            <button className={styles.viewbutton}> View</button>
-          </div>
-        </div>
+        <table className={styles.reqtable}>
+          <tr>
+            <th>Project Name</th>
+            <th>Checkout Time</th>
+            <th>Return Time</th>
+            <th>Status</th>
+          </tr>
+          <tr className={styles.projectreq}>
+            <td>Fake Past Project Name 1</td>
+            <td>02/20/20 12:00pm</td>
+            <td>03/20/20 1:00pm</td>
+            <td>Pending</td>
+            <td>
+              <button className={styles.viewbutton}> View</button>
+            </td>
+            <td>
+              <button className={styles.editbutton}> Edit</button>
+            </td>
+          </tr>
+          <tr>
+            <th>Project Name</th>
+            <th>Checkout Time</th>
+            <th>Return Time</th>
+            <th>Status</th>
+          </tr>
+          <tr className={styles.projectreq}>
+            <td>Fake past Project Name 2</td>
+            <td>01/19/20 3:00pm</td>
+            <td>02/15/20 4:00pm</td>
+            <td>Approved</td>
+            <td>
+              <button className={styles.viewbutton}> View</button>
+            </td>
+            <td>
+              <button className={styles.editbutton}> Edit</button>
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
   );
