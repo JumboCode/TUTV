c8570fc34b211153f5a62e0700065f501a31dccc
erenaci <erenacinoue@gmail.com>
2020-02-29 11:43:30 -0500

    very very slight update

--- client/src/pages/AdminCheckout/AdminCheckout.tsx
+++ client/src/pages/AdminCheckout/AdminCheckout.tsx
@@ -1,1 +1,10 @@
 import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import styles from './AdminCheckout.module.css';
+
+const AdminCheckout: React.FC = () => {
+  return <h1>A Lover's Quarrel</h1>;
+};
+
+export default AdminCheckout;
