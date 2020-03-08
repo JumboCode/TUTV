c4f63d8e058c909d9c19df8e34ebf4b03ae1910f
Luke Taylor <luke@deentaylor.com>
2020-03-08 18:07:37 -0400

    Render ModalDialog component inside the modal container

--- client/src/components/ModalDialog/ModalDialog.tsx
+++ client/src/components/ModalDialog/ModalDialog.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-import styles from './Button.module.css';
+import styles from './ModalDialog.module.css';
 
 type ModalProps = {
   displayed: boolean;
@@ -8,10 +9,19 @@ type ModalProps = {
 };
 
 const ModalDialog: React.FC<ModalProps> = ({ displayed, children }) => {
-  return (
-    <div className="scrim">
-      <div className="dialog">{children}</div>
-    </div>
+  const wrapperClassName = [
+    styles.modalWrapper,
+    displayed ? styles.displayed : styles.hidden
+  ].join(' ');
+
+  return ReactDOM.createPortal(
+    <div className={wrapperClassName}>
+      <div className={styles.scrim}>
+        <div className={styles.dialog}>{children}</div>
+      </div>
+    </div>,
+    // Render inside the top-level modal container element
+    document.getElementById('modal-container') as HTMLElement
   );
 };
 
