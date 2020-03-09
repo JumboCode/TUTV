d78166a2c58f46463a73f80a8f76d7a3544570e6
Luke Taylor <luke@deentaylor.com>
2020-03-08 21:53:47 -0400

    Update styleguidist config to support modals

--- client/styleguide.config.js
+++ client/styleguide.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   components: 'src/components/**/[A-Z]*.{js,jsx,ts,tsx}',
-  webpackConfig: require('./config/webpack.config.js')
+  webpackConfig: require('./config/webpack.config.js'),
+  // Enable modals to display properly inside style guide
+  template: {
+    body: {
+      raw: '<div id="modal-container"></div>'
+    }
+  }
 };
