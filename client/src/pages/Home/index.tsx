1d3bc1fa980e986ee9591a74e3f6970dae932a12
Luke Taylor <luke@deentaylor.com>
2020-04-10 01:05:54 -0400

    Add basic 'useApi' example to homepage
    authentication to come

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import useApi from '../../api';
 
 const Home: React.FC = () => {
+  const { data } = useApi(' ', {});
+
   return (
     <div>
       <h1>Home page</h1>
+      {JSON.stringify(data)}
     </div>
   );
 };
