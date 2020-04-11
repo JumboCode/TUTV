d70851665ee67e620b21d5e84e21fdb0621ddf35
Frank Ma <ma990801@gmail.com>
2020-04-11 14:30:20 -0400

    Added user to equipment request

--- server/api/models.py
+++ server/api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+from django.contrib.auth.models import User
+
 """
 ENCAPSULATION OF MODELS:
     CATEGORY --> TYPE --> ITEM
@@ -72,7 +74,7 @@ class EquipmentRequest(models.Model):
     request_out = models.DateTimeField(blank=True, null=True)
     request_in = models.DateTimeField(blank=True, null=True)
     equipment_items = models.ManyToManyField(EquipmentItem, related_name='linked_requests') # no on_delete option possible. What happens when an item is deleted?
-    # user = # TODO 
+    user = models.ForeignKey(User, related_name='requests', on_delete=models.SET_NULL, null=True) # when the referenced object is deleted (i.e. the User), do not delete the request, but rather set the ForeignKey to null. This is only possible if null is True. 
 
     # def save(self, *args, **kwargs):
     #     request = super(EquipmentRequest, self).save(*args, **kwargs)
