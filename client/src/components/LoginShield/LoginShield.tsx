faee6fc5a6afee4114d865f85be5adcee7b14d87
Luke Taylor <luke@deentaylor.com>
2020-04-11 15:31:38 -0400

    Add LoginShield component
    This component replaces its children with a provided fallback when the
    user is not logged in.

---
+++ client/src/components/LoginShield/LoginShield.tsx
@@ -0,0 +1,18 @@
+import React, { ReactElement } from 'react';
+import { useStore } from '../../store';
+
+export function useLoginState() {
+  const { state } = useStore();
+  return state.auth.accessToken && state.auth.refreshToken;
+}
+
+interface LoginShieldProps {
+  fallback: ReactElement;
+}
+
+const LoginShield: React.FC<LoginShieldProps> = ({ children, fallback }) => {
+  const isLoggedIn = useLoginState();
+  return isLoggedIn ? (children as ReactElement) : fallback;
+};
+
+export default LoginShield;
