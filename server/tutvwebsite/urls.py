66e3a22fc621f8eb284b137da99b6fa4b6899007
Frama-99 <jiayi.ma@tufts.edu>
2020-04-12 21:35:55 -0400

    Restore catch-all for front-end
    Co-Authored-By: Deepanshu Utkarsh <duci9y@users.noreply.github.com>

--- server/tutvwebsite/urls.py
+++ server/tutvwebsite/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('api/v1/', include('api.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # re_path(r'^', views.FrontendAppView.as_view())
+    re_path(r'^', views.FrontendAppView.as_view())
 ]
