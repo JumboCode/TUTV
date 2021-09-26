f89ec8b7a435301d7074f438611da0fcfe4f4d67
Frama-99 <ma990801@gmail.com>
2021-09-26 00:19:48 -0400

    Replaced react-collapsible with MUI accordion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,69 +14,74 @@ import AdminDashboard from './pages/admin/AdminDashboard';
 import RequestSummary from './pages/admin/RequestSummary';
 import SignIn from './pages/SignIn';
 
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
 const App: React.FC = () => {
   return (
-    <Router>
-      <StoreProvider>
-        <LoginShield fallback={<SignIn />}>
-          <div className="App">
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/catalog">Equipment Catalog</Link>
-                </li>
-                <li>
-                  <Link to="/equipmentbrowser">Equipment Browser</Link>
-                </li>
-                <li>
-                  <Link to="/memberdashboard">MemberDashboard</Link>
-                </li>
-                <li>
-                  <Link to="/admin/dashboard">Admin Dashboard</Link>
-                </li>
-                <li>
-                  <Link to="/admin/equipment-request">Equipment Request</Link>
-                </li>
-              </ul>
-            </nav>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Router>
+        <StoreProvider>
+          <LoginShield fallback={<SignIn />}>
+            <div className="App">
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/catalog">Equipment Catalog</Link>
+                  </li>
+                  <li>
+                    <Link to="/equipmentbrowser">Equipment Browser</Link>
+                  </li>
+                  <li>
+                    <Link to="/memberdashboard">MemberDashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/admin/dashboard">Admin Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/admin/equipment-request">Equipment Request</Link>
+                  </li>
+                </ul>
+              </nav>
 
-            {React.createElement(function LogOutButton() {
-              const { dispatch } = useStore();
-              const logout = () => dispatch({ type: 'logout' });
-              return (
-                <Button variant="gray" onClick={logout}>
-                  Log out
-                </Button>
-              );
-            })}
+              {React.createElement(function LogOutButton() {
+                const { dispatch } = useStore();
+                const logout = () => dispatch({ type: 'logout' });
+                return (
+                  <Button variant="gray" onClick={logout}>
+                    Log out
+                  </Button>
+                );
+              })}
 
-            <Switch>
-              <Route path="/admin/dashboard">
-                <AdminDashboard />
-              </Route>
-              <Route path="/catalog">
-                <Catalog />
-              </Route>
-              <Route path="/equipmentbrowser">
-                <EquipmentBrowser />
-              </Route>
-              <Route path="/memberdashboard">
-                <MemberDashboard />
-              </Route>
-              <Route path="/admin/equipment-request">
-                <RequestSummary />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-        </LoginShield>
-      </StoreProvider>
-    </Router>
+              <Switch>
+                <Route path="/admin/dashboard">
+                  <AdminDashboard />
+                </Route>
+                <Route path="/catalog">
+                  <Catalog />
+                </Route>
+                <Route path="/equipmentbrowser">
+                  <EquipmentBrowser />
+                </Route>
+                <Route path="/memberdashboard">
+                  <MemberDashboard />
+                </Route>
+                <Route path="/admin/equipment-request">
+                  <RequestSummary />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+          </LoginShield>
+        </StoreProvider>
+      </Router>
+    </LocalizationProvider>
   );
 };
 
--- client/src/pages/EquipmentBrowser/EquipmentBrowser.tsx
+++ client/src/pages/EquipmentBrowser/EquipmentBrowser.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './EquipmentBrowser.module.css';
-import Collabspible from 'react-collapsible';
 import Button from 'components/Button';
-import { ChevronDown } from 'react-feather';
 import Item from 'types/Item';
 import EquipmentGrid from 'components/EquipmentGrid';
+
+import TextField from '@mui/material/TextField';
+import DateTimePicker from '@mui/lab/DateTimePicker';
+
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const EquipmentBrowser: React.FC = () => {
   const [items, setItems] = React.useState<Array<Item>>([]);
@@ -14,23 +21,32 @@ const EquipmentBrowser: React.FC = () => {
       .then((response) => setItems(response.data))
       .catch((error) => console.error(error));
   }, []);
+  const [value, setValue] = React.useState<Date | null>(new Date());
   return (
     <div>
       <div className={styles.header}>
         <table className={styles.tableClass}>
           <tbody>
-            <tr>
-              <th>Project Name</th>
-              <th>Checkout Time</th>
-              <th>Return Time</th>
-            </tr>
             <tr className={styles.projectreq}>
-              <td className={styles.cellName}>Fake Project Name</td>
               <td>
-                <input type="datetime-local" />
+                <DateTimePicker
+                  renderInput={(props) => <TextField {...props} />}
+                  label="Checkout Time"
+                  value={value}
+                  onChange={(newValue) => {
+                    setValue(newValue);
+                  }}
+                />
               </td>
               <td>
-                <input type="datetime-local" />
+                <DateTimePicker
+                  renderInput={(props) => <TextField {...props} />}
+                  label="Return Time"
+                  value={value}
+                  onChange={(newValue) => {
+                    setValue(newValue);
+                  }}
+                />
               </td>
             </tr>
           </tbody>
@@ -43,58 +59,58 @@ const EquipmentBrowser: React.FC = () => {
       <div>
         <div className={styles.equipmentPage}>
           <div className={styles.wrapper}>
-            <Collabspible
-              trigger={
-                <React.Fragment>
-                  {'Camera'}
-                  <ChevronDown color="white" size={20} />
-                </React.Fragment>
-              }
-              triggerTagName="div"
-              className={styles.collapsehead}
-              openedClassName={styles.collapsehead}
-            >
-              <EquipmentGrid items={items} />
-            </Collabspible>
-            <Collabspible
-              trigger={
-                <React.Fragment>
-                  {'Audio'}
-                  <ChevronDown color="white" size={20} />
-                </React.Fragment>
-              }
-              triggerTagName="div"
-              className={styles.collapsehead}
-              openedClassName={styles.collapsehead}
-            >
-              <EquipmentGrid items={items} />
-            </Collabspible>
-            <Collabspible
-              trigger={
-                <React.Fragment>
-                  {'Lighting'}
-                  <ChevronDown color="white" size={20} />
-                </React.Fragment>
-              }
-              triggerTagName="div"
-              className={styles.collapsehead}
-              openedClassName={styles.collapsehead}
-            >
-              <EquipmentGrid items={items} />
-            </Collabspible>
-            <Collabspible
-              trigger={
-                <React.Fragment>
-                  {'Misc'}
-                  <ChevronDown color="white" size={20} />
-                </React.Fragment>
-              }
-              triggerTagName="div"
-              className={styles.collapsehead}
-              openedClassName={styles.collapsehead}
-            >
-              <EquipmentGrid items={items} />
-            </Collabspible>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Camera and Lenses</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>Items should be displayed below:</Typography>
+                <EquipmentGrid items={items} />
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Audio</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>Items should be displayed below:</Typography>
+                <EquipmentGrid items={items} />
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel3a-content"
+                id="panel3a-header"
+              >
+                <Typography>Lighting</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>Items should be displayed below:</Typography>
+                <EquipmentGrid items={items} />
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel3a-content"
+                id="panel3a-header"
+              >
+                <Typography>Misc</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>Items should be displayed below:</Typography>
+                <EquipmentGrid items={items} />
+              </AccordionDetails>
+            </Accordion>
           </div>
           <div className={styles.cart}>
             <div>
