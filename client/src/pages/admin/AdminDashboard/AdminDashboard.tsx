20fb98e37f9cc8ba127a1b56abc517a332d37224
Frank Ma <ma990801@gmail.com>
2022-01-16 13:33:45 -0500

    Display all request cards on admin dashboard

--- client/src/pages/admin/AdminDashboard/AdminDashboard.tsx
+++ client/src/pages/admin/AdminDashboard/AdminDashboard.tsx
@@ -6,28 +6,28 @@ import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
-
-import AddIcon from '@mui/icons-material/Add';
 import CircularProgress from '@mui/material/CircularProgress';
 
+import { useApiRequest } from 'api';
+
+import { EquipmentRequest } from 'types/Request';
+import RequestCard from 'components/RequestCard';
+
 const AdminDashboard: React.FC = () => {
-  const statuses = [
-    'Requested',
-    'Confirmed',
-    'Signed Out',
-    'Overdue',
-    'Returned',
-    'Cancelled',
-  ];
-  const [statusFilter, setStatusFilter] = React.useState([
-    'Requested',
-    'Confirmed',
-    'Signed Out',
-    'Overdue',
-  ]);
-  const handleStatusFilterChange = (event: any) => {
-    setStatusFilter(event.target.value as string[]);
-  };
+  const [requests, setRequests] = React.useState<EquipmentRequest[]>([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  // TODO: Apply filtering
+  const getRequests = useApiRequest('equipment-requests');
+  React.useEffect(() => {
+    getRequests()
+      .then((requests) => {
+        setRequests(requests);
+        setIsLoading(false);
+      })
+      // TODO: better error handling
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <Stack spacing={2} sx={{ minWidth: '50%', marginBottom: '50px' }}>
@@ -40,6 +40,18 @@ const AdminDashboard: React.FC = () => {
       >
         <Typography variant="h5">Member Requests</Typography>
       </Box>
+      {isLoading ? (
+        <Box sx={{ alignSelf: 'center' }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Stack spacing={2}>
+          {requests.length !== 0 &&
+            requests.map((request: EquipmentRequest) => (
+              <RequestCard request={request}></RequestCard>
+            ))}
+        </Stack>
+      )}
     </Stack>
   );
 };
